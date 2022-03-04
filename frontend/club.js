447fe0c846f57c9bd7248407d010303d62c7153e
Jessica <jessicaluu241@gmail.com>
2022-03-04 15:06:03 -0800

    updated spelling errors in front end and created connecting to database to fill description page

--- frontend/club.js
+++ frontend/club.js
@@ -3,25 +3,22 @@
 
 var baseURL = 'http://localhost:8080/'
 
-async function editName() {
-
-    let name = 'Japanese Student Association'
+async function editDescription(){
+    let name = 'Key Club'
 
     let clubs = await axios.get(baseURL+'get-club/'+name)
 
     let clubName = document.querySelector('#club-names')
     clubName.innerHTML = clubs.data[0].name
+
+    let clubRoom = document.querySelector('#club-room')
+    clubRoom.innerHTML = "Room " + clubs.data[0].room
+
+    let clubLocation = document.querySelector('#location')
+    clubLocation.innerHTML = "Where: Room " + clubs.data[0].room + ", " + clubs.data[0].advisor
+
+    let clubTime = document.querySelector('#time')
+    clubTime.innerHTML = "When: " + clubs.data[0].days + ", " + clubs.data[0].time
 }
 
-async function editRoom(){
-
-    let name = 'Japanese Student Association'
-
-    let clubs = await axios.get(baseURL+'get-club/'+name)
-
-    let clubName = document.querySelector('#club-room')
-    clubName.innerHTML = clubs.data[0].room
-}
-
-editName()
-editRoom()
+editDescription()
