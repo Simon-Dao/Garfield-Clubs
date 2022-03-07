9936e777c838bb69bc19df9b71661e3c934ee43e
Jessica <jessicaluu241@gmail.com>
2022-03-07 15:33:00 -0800

    added cookies to fill the individual club pages with proper club information

--- frontend/club.js
+++ frontend/club.js
@@ -3,8 +3,24 @@
 
 var baseURL = 'http://localhost:8080/'
 
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+     for(let i = 0; i < ca.length; i++) {
+         let c = ca[i];
+     while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+}
+    if (c.indexOf(name) == 0) {
+    return c.substring(name.length, c.length);
+    }
+}
+    return "";
+}
 async function editDescription(){
-    let name = 'Key Club'
+    let name = getCookie("club");
 
     let clubs = await axios.get(baseURL+'get-club/'+name)
 
